34b19873af11750128a1b62d12b6942e732c24d2
Gideon Maydell <gideon@maydell.at>
2022-12-01 16:05:24 +0100

    fixed bugs all over website

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "fridges#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :fridges, only: %i[index show] do
     resources :ingredients
   end
+  resources :ingredients, only: %i[destroy]
+
   resources :recipes do
     resources :reviews, only: %i[index new create]
   end
