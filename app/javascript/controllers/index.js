23d020ca9771be4c55b76393567c64720c6e59e2
Benjamin Boulle <benjaminboulle.sjb@gmail.com>
2022-12-05 17:37:17 +0100

    swipewsipeswipe

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -18,3 +18,6 @@ application.register("hello", HelloController)
 
 import StarRatingController from "./star_rating_controller"
 application.register("star-rating", StarRatingController)
+
+import SwitchController from "./switch_controller"
+application.register("switch", SwitchController)
---
+++ app/javascript/controllers/switch_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="switch"
+export default class extends Controller {
+  static targets = ["the_switch", "home_fridge", "away_fridge"];
+
+  connect() {
+    console.log("The 'switch' controller is now loaded!");
+    console.log("Nice!!!!");
+  }
+
+  switch_content(event) {
+    if (this.the_switchTarget.checked) {
+      this.home_fridgeTarget.classList.remove("d-none")
+      this.home_fridgeTarget.classList.add("d-block")
+      this.away_fridgeTarget.classList.remove("d-block")
+      this.away_fridgeTarget.classList.add("d-none")
+    } else {
+      this.home_fridgeTarget.classList.remove("d-block")
+      this.home_fridgeTarget.classList.add("d-none")
+      this.away_fridgeTarget.classList.remove("d-none")
+      this.away_fridgeTarget.classList.add("d-block")
+    }
+  }
+
+
+}
